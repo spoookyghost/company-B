797dd6e8d6b2d35d7bbf9282b2ce91a44dd540ac
spoookyghost <52107169+spoookyghost@users.noreply.github.com>
2023-04-03 14:46:57 +0200

    Update main_query.sql

--- main_query.sql
+++ main_query.sql
@@ -1,62 +1,89 @@
-WITH 
-D AS 
-(SELECT EPCs FROM X
-WHERE 
-(SELECT epcs_0 FROM Y WHERE reserve_quick_chabert = "RESERVE") IN (epcs_de_zone_1, epcs_de_zone_2, epcs_de_zone_3, epcs_de_zone_4, epcs_de_zone_5, epcs_de_zone_6, epcs_de_zone_7, epcs_de_zone_8, epcs_de_zone_9, epcs_de_zone_10) 
-AND 
-DATE(datetime_fin) = (SELECT MAX(DATE(datetime_fin)) FROM X WHERE (SELECT epcs_0 FROM Y WHERE reserve_quick_chabert = "RESERVE") IN (epcs_de_zone_1, epcs_de_zone_2, epcs_de_zone_3, epcs_de_zone_4, epcs_de_zone_5, epcs_de_zone_6, epcs_de_zone_7, epcs_de_zone_8, epcs_de_zone_9, epcs_de_zone_10))),
 
-E AS 
-(SELECT EPCs FROM X 
-WHERE 
-(SELECT epcs_0 FROM Y WHERE reserve_quick_chabert = "REBUT") IN (epcs_de_zone_1, epcs_de_zone_2, epcs_de_zone_3, epcs_de_zone_4, epcs_de_zone_5, epcs_de_zone_6, epcs_de_zone_7, epcs_de_zone_8, epcs_de_zone_9, epcs_de_zone_10) 
-AND 
-DATE(datetime_fin) = (SELECT MAX(DATE(datetime_fin)) FROM X WHERE (SELECT epcs_0 FROM Y WHERE reserve_quick_chabert = "REBUT") IN (epcs_de_zone_1, epcs_de_zone_2, epcs_de_zone_3, epcs_de_zone_4, epcs_de_zone_5, epcs_de_zone_6, epcs_de_zone_7, epcs_de_zone_8, epcs_de_zone_9, epcs_de_zone_10)))
 
-SELECT CURRENT_DATETIME() AS datetime_upload, epcs_0 as EPCs, ean, description,
+CREATE OR REPLACE TABLE FUNCTION A.B(resto STRING)
+AS (WITH 
+E  AS 
+(SELECT EPCs FROM `C`
+WHERE 
+(SELECT epcs_0 FROM `D` WHERE type = "E " AND restaurant = resto) IN (epcs_de_zone_1, epcs_de_zone_2, epcs_de_zone_3, epcs_de_zone_4, epcs_de_zone_5, epcs_de_zone_6, epcs_de_zone_7, epcs_de_zone_8, epcs_de_zone_9, epcs_de_zone_10)
+AND 
+DATE(datetime_fin) = (SELECT MAX(DATE(datetime_fin)) FROM `B` WHERE (SELECT epcs_0 FROM `C` WHERE type = "E " AND restaurant = resto) IN (epcs_de_zone_1, epcs_de_zone_2, epcs_de_zone_3, epcs_de_zone_4, epcs_de_zone_5, epcs_de_zone_6, epcs_de_zone_7, epcs_de_zone_8, epcs_de_zone_9, epcs_de_zone_10)) 
+AND restaurant = resto),
+
+F AS 
+(SELECT EPCs FROM `B`  
+WHERE 
+(SELECT epcs_0 FROM `C` WHERE type = "REBUT" AND restaurant = resto) IN (epcs_de_zone_1, epcs_de_zone_2, epcs_de_zone_3, epcs_de_zone_4, epcs_de_zone_5, epcs_de_zone_6, epcs_de_zone_7, epcs_de_zone_8, epcs_de_zone_9, epcs_de_zone_10) 
+AND 
+DATE(datetime_fin) = (SELECT MAX(DATE(datetime_fin)) FROM `B` WHERE (SELECT epcs_0 FROM `C` WHERE type = "REBUT" AND restaurant = resto) IN (epcs_de_zone_1, epcs_de_zone_2, epcs_de_zone_3, epcs_de_zone_4, epcs_de_zone_5, epcs_de_zone_6, epcs_de_zone_7, epcs_de_zone_8, epcs_de_zone_9, epcs_de_zone_10)) 
+AND restaurant = resto),
+
+G AS 
+(SELECT
+EPCs, datetime_fin
+FROM (SELECT ROW_NUMBER () OVER (PARTITION BY EPCs ORDER BY datetime_fin DESC) AS row_number, EPCs, datetime_fin FROM `B` 
+WHERE restaurant = resto)
+WHERE row_number = 1),
+
+H AS (SELECT EPCs, datetime_fin FROM `B`  
+WHERE 
+(SELECT epcs_0 FROM `C` WHERE type = "REBUT" AND restaurant = resto) IN (epcs_de_zone_1, epcs_de_zone_2, epcs_de_zone_3, epcs_de_zone_4, epcs_de_zone_5, epcs_de_zone_6, epcs_de_zone_7, epcs_de_zone_8, epcs_de_zone_9, epcs_de_zone_10)
+AND restaurant = resto),
+
+I AS (SELECT rebut.EPCs AS EPCs FROM rebut
+LEFT JOIN last_inventory
+ON rebut.EPCs = last_inventory.EPCs
+AND rebut.datetime_fin = last_inventory.datetime_fin
+WHERE last_inventory.EPCs IS NOT NULL)
+
+SELECT CURRENT_DATETIME() AS datetime_upload, epcs_0 as EPCs, ean, description, SITE,
 
 CASE
-  WHEN epcs_0 IN (SELECT EPCs FROM X
+  WHEN epcs_0 IN (SELECT EPCs FROM I) AND epcs_0 NOT IN (SELECT E )
+
+THEN “Z”
+
+  WHEN epcs_0 IN (SELECT EPCs FROM `B`
 WHERE
-datetime_fin >= DATE_SUB((SELECT max(datetime_fin) as date FROM X WHERE Statut = "PROPRE"), INTERVAL 40 hour) AND Statut = "PROPRE")
-  AND epcs_0 NOT IN (SELECT * FROM D)
-  AND epcs_0 NOT IN (SELECT * FROM E)
+datetime_fin >= DATE_SUB((SELECT max(datetime_fin) as date FROM `B` WHERE Statut = "PROPRE" AND restaurant = resto), INTERVAL 40 hour) AND Statut = "PROPRE" AND restaurant = resto)
+  AND epcs_0 NOT IN (SELECT * FROM E )
+  AND epcs_0 NOT IN (SELECT * FROM F)
   
-THEN "A"
+THEN “Y”
 
   WHEN epcs_0 
-  IN (SELECT EPCs FROM X
+  IN (SELECT EPCs FROM `B`
 WHERE
-datetime_fin < DATE_SUB((SELECT max(datetime_fin) as date FROM X WHERE Statut = "PROPRE"), INTERVAL 40 hour)
+datetime_fin < DATE_SUB((SELECT max(datetime_fin) as date FROM `B` WHERE Statut = "PROPRE" AND restaurant = resto), INTERVAL 40 hour)
 AND
-datetime_fin >= DATE_SUB((SELECT max(datetime_fin) as date FROM X WHERE Statut = "PROPRE"), INTERVAL 64 hour) AND Statut = "PROPRE")
-  AND epcs_0 NOT IN (SELECT * FROM D)
-  AND epcs_0 NOT IN (SELECT * FROM E)
-THEN "B"
+datetime_fin >= DATE_SUB((SELECT max(datetime_fin) as date FROM `B` WHERE Statut = "PROPRE" AND restaurant = resto), INTERVAL 64 hour) AND Statut = "PROPRE" AND restaurant = resto)
+  AND epcs_0 NOT IN (SELECT * FROM E )
+  AND epcs_0 NOT IN (SELECT * FROM F)
+THEN “X”
 
   WHEN epcs_0 
-  IN (SELECT EPCs FROM X
+  IN (SELECT EPCs FROM `B`
 WHERE
-DATETIME(datetime_fin) < DATE_SUB((SELECT max(datetime_fin) as date FROM X WHERE Statut = "PROPRE"), INTERVAL 64 hour) AND Statut = "PROPRE")
-  AND epcs_0 NOT IN (SELECT * FROM D)
-  AND epcs_0 NOT IN (SELECT * FROM E)
-THEN "C"
+DATETIME(datetime_fin) < DATE_SUB((SELECT max(datetime_fin) as date FROM `B` WHERE Statut = "PROPRE" AND restaurant = resto), INTERVAL 64 hour) AND Statut = "PROPRE" AND restaurant = resto)
+  AND epcs_0 NOT IN (SELECT * FROM E )
+  AND epcs_0 NOT IN (SELECT * FROM F)
+THEN “W”
 
   WHEN
-  epcs_0 IN (SELECT * FROM D)
-  AND epcs_0 NOT IN (SELECT * FROM E)
-THEN "D"
+  epcs_0 IN (SELECT * FROM E )
+  AND epcs_0 NOT IN (SELECT * FROM F)
+THEN “V”
 
-WHEN epcs_0 NOT IN (SELECT * FROM D)
-  AND epcs_0 IN (SELECT * FROM E)
-THEN "E"
+WHEN epcs_0 NOT IN (SELECT * FROM E )
+  AND epcs_0 IN (SELECT * FROM F)
+THEN “Z”
 
   WHEN epcs_0
-  NOT IN (SELECT EPCs FROM X)
-THEN "F"
+  NOT IN (SELECT EPCs FROM `B` WHERE restaurant = resto)
+THEN “U”
 
-  ELSE "F"
+  ELSE “Z”
    
   END as activity
 
-  FROM Y
+  FROM `D` WHERE SITE = resto)
